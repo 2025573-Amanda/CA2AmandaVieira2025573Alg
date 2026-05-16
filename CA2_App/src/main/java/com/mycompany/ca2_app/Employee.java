58c56e7aa3d09ec80c07f79d8587bf0a6f90a71a
2025573-Amanda <2025573@student.cct.ie>
2026-05-16 14:05:17 +0100

    Employee class with full documentation and field-level comments

--- CA2_App/src/main/java/com/mycompany/ca2_app/Employee.java
+++ CA2_App/src/main/java/com/mycompany/ca2_app/Employee.java
@@ -5,9 +5,70 @@
 package com.mycompany.ca2_app;
 
 /**
+ * Represents an Employee in the bank system.
+ * Each employee has:
+ * - A full name
+ * - A ManagerType (enum)
+ * - A DepartmentType (enum)
  *
- * @author home
+ * This class is used when creating Employee records
+ * and when inserting them into the Binary Tree.
  */
 public class Employee {
-    
+
+    // Stores the full name of the employee
+    private String fullName;
+
+    // Stores the manager type (enum) assigned to the employee
+    private ManagerType managerType;
+
+    // Stores the department (enum) where the employee works
+    private DepartmentType departmentType;
+
+    /**
+     * Constructor used to create a new Employee object.
+     *
+     * @param fullName The employee's full name
+     * @param managerType The type of manager supervising the employee
+     * @param departmentType The department where the employee works
+     */
+    public Employee(String fullName, ManagerType managerType, DepartmentType departmentType) {
+        this.fullName = fullName;
+        this.managerType = managerType;
+        this.departmentType = departmentType;
+    }
+
+    /**
+     * Returns the employee's full name.
+     */
+    public String getFullName() {
+        return fullName;
+    }
+
+    /**
+     * Returns the manager type assigned to the employee.
+     */
+    public ManagerType getManagerType() {
+        return managerType;
+    }
+
+    /**
+     * Returns the department where the employee works.
+     */
+    public DepartmentType getDepartmentType() {
+        return departmentType;
+    }
+
+    /**
+     * Returns a formatted string representation of the Employee object.
+     */
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "fullName='" + fullName + '\'' +
+                ", managerType=" + managerType +
+                ", departmentType=" + departmentType +
+                '}';
+    }
 }
+
