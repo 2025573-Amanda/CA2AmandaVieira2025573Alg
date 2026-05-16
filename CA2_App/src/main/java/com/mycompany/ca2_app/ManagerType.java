f65879ace46083ef3315cacb6fc8bdb813257304
2025573-Amanda <2025573@student.cct.ie>
2026-05-16 14:22:12 +0100

    Corrected ManagerType implementation by changing class to enum and documenting all roles.

---
+++ CA2_App/src/main/java/com/mycompany/ca2_app/ManagerType.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ca2_app;
+/**
+ * Enum representing the different types of managers
+ * that an Employee can be assigned to.
+ *
+ * This is used when creating Employee records
+ * and when inserting them into the Binary Tree.
+ */
+/**
+ *
+ * @author home
+ */
+public enum ManagerType {
+       // Represents a Junior Manager
+    JUNIOR,
+
+    // Represents a Senior Manager
+    SENIOR,
+
+    // Represents a Team Lead
+    TEAM_LEAD,
+
+    // Represents a Department Manager
+    DEPARTMENT_MANAGER
+}
+
