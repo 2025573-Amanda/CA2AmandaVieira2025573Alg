1f3b864e7d374d01386f7240bb912262322fc426
2025573-Amanda <2025573@student.cct.ie>
2026-05-17 11:41:10 +0100

    Add final ApplicationMain.java defines the main entry point and starts the CA2_App .

--- CA2_App/src/main/java/com/mycompany/ca2_app/ApplicationMain.java
+++ CA2_App/src/main/java/com/mycompany/ca2_app/ApplicationMain.java
@@ -9,5 +9,20 @@ package com.mycompany.ca2_app;
  * @author home
  */
 public class ApplicationMain {
-    
+    /**
+ * Entry point of the CA2 Bank Management System.
+ * This class only contains the main() method,
+ * which starts the CA2_App menu.
+ */
+
+    public static void main(String[] args) {
+
+        // Create the main application object
+        CA2_App app = new CA2_App();
+
+        // Start the menu system
+        app.start();
+    }
 }
+    
+    
