0351a215e17838aea902abd9e1e9131dc52d4547
2025573-Amanda <2025573@student.cct.ie>
2026-05-20 10:42:22 +0100

    Add elements/ fix error

--- CA2_App/src/main/java/com/mycompany/ca2_app/MenuOptionEnum.java
+++ CA2_App/src/main/java/com/mycompany/ca2_app/MenuOptionEnum.java
@@ -9,10 +9,14 @@ package com.mycompany.ca2_app;
  * @author home
  */
 public enum MenuOptionEnum {
-    IMPORT_APPLICANTS,
+   IMPORT_APPLICANTS,
     SORT_APPLICANTS,
     SEARCH_APPLICANTS,
     ADD_NEW_EMPLOYEE,
     GENERATE_BINARY_TREE,
+    PRINT_TREE_IN_ORDER,
+    PRINT_TREE_LEVEL_ORDER,
+    SHOW_TREE_HEIGHT,
+    SHOW_NODE_COUNT,
     EXIT_SYSTEM
 }
